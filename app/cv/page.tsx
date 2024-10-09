c7a2f8e36ae019e3e07e2c2e6709d77394953b25
siddartha-10 <pullakhandam.siddartha@gmail.com>
2024-10-09 12:12:31 -0400

    cv link

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -6,7 +6,7 @@ export default function CV() {
 
   useEffect(() => {
     // Replace with your Google Drive CV link
-    const googleDriveLink = 'https://drive.google.com/your-cv-link';
+    const googleDriveLink = "https://drive.google.com/file/d/1ZeEP0oP7Fm_ZcE4wYwf8H4AX8fIxlBlv/view?usp=sharing";
     // Redirect to the Google Drive link
     window.location.href = googleDriveLink;
   }, [router]);
